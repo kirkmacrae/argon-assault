6109c5f73575fae3628f4eec393750dfe6d47c50
Kirk MacRae <kirk.macrae@gmail.com>
2020-10-07 13:50:00 -0400

    tidy up folders and scripts

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,29 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Splash : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
     [SerializeField] float levelLoadDelay = 4f;
 
-    private void Awake()
-    {
-        DontDestroyOnLoad(this.gameObject);
-    }
-
     // Start is called before the first frame update
     void Start()
-    {        
+    {
         Invoke("StartDelay", levelLoadDelay);
     }
 
     private void StartDelay()
     {
         SceneManager.LoadScene(1);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
