d8fb33025212dc40b6a4e56dae31634f56d46832
Kirk MacRae <kirk.macrae@gmail.com>
2020-10-13 19:42:35 -0400

    shoot enemies get points

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{    
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+
+    ScoreBoard scoreBoard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider enemyBoxCollider = gameObject.AddComponent<BoxCollider>();
+        enemyBoxCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        scoreBoard.ScoreHit(scorePerHit);
+        
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    [SerializeField] float deathCountdown = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, deathCountdown);
+    }
+}
